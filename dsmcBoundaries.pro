a4c2e34c5c25be34b51f5741128cb09894909973
alex <chudinov@inbox.ru>
2020-11-05 13:12:53 +0300

    init

---
+++ dsmcBoundaries.pro
@@ -0,0 +1,103 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2020-09-16T15:21:52
+#
+#-------------------------------------------------
+
+QT       -= core gui
+
+TARGET = dsmcFoam
+TEMPLATE = app
+
+DEFINES += DSMCPROJECT_LIBRARY
+
+# The following define makes your compiler emit warnings if you use
+# any feature of Qt which has been marked as deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if you use deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    BinaryCollisionModel.C \
+    BinaryCollisionModelNew.C \
+    defineDSMCParcel.C \
+    DSMCCloud.C \
+    DSMCCloudName.C \
+    dsmcFoam.C \
+    dsmcParcel.C \
+    DSMCParcel.C \
+    DSMCParcelIO.C \
+    FreeStream.C \
+    InflowBoundaryModel.C \
+    InflowBoundaryModelNew.C \
+    LarsenBorgnakkeVariableHardSphere.C \
+    makeDSMCParcelBinaryCollisionModels.C \
+    makeDSMCParcelInflowBoundaryModels.C \
+    makeDSMCParcelWallInteractionModels.C \
+    MaxwellianThermal.C \
+    MixedDiffuseSpecular.C \
+    NoBinaryCollision.C \
+    NoInflow.C \
+    SpecularReflection.C \
+    VariableHardSphere.C \
+    WallInteractionModel.C \
+    WallInteractionModelNew.C
+
+HEADERS += \
+    BinaryCollisionModel.H \
+    dsmcCloud.H \
+    DSMCCloud.H \
+    DSMCCloudI.H \
+    dsmcParcel.H \
+    DSMCParcel.H \
+    DSMCParcelI.H \
+    FreeStream.H \
+    InflowBoundaryModel.H \
+    LarsenBorgnakkeVariableHardSphere.H \
+    MaxwellianThermal.H \
+    MixedDiffuseSpecular.H \
+    NoBinaryCollision.H \
+    NoInflow.H \
+    SpecularReflection.H \
+    VariableHardSphere.H \
+    WallInteractionModel.H \
+    inputstream.h \
+    createFields.H
+
+unix {
+    target.path = /usr/lib
+    INSTALLS += target
+}
+
+DEFINES += \
+    linux64 \
+    WM_ARCH_OPTION=64 \
+    WM_DP \
+    WM_LABEL_SIZE=32 \
+    NoRepository \
+
+INCLUDEPATH += \
+    . \
+    /opt/openfoam-dev/src/finiteVolume/lnInclude \
+    /opt/openfoam-dev/src/lagrangian/basic/lnInclude \
+    /opt/openfoam-dev/src/meshTools/lnInclude \
+    /opt/openfoam-dev/src/OpenFOAM/lnInclude \
+    /opt/openfoam-dev/src/OSspecific/POSIX/lnInclude
+
+QMAKE_CXXFLAGS += \
+    -std=c++14 \
+    -m64 \
+    -Wall -Wextra -Wold-style-cast -Wnon-virtual-dtor -Wno-unused-parameter \
+    -Wno-invalid-offsetof -Wno-attributes -O3  -ftemplate-depth-100 \
+    -fPIC -c
+
+QMAKE_LIBDIR += /opt/openfoam-dev/platforms/linux64GccDPInt32Opt/lib \
+    /opt/openfoam-dev/platforms/linux64GccDPInt32Opt/lib/dummy
+
+LIBS += -lmeshTools -lfiniteVolume -llagrangian -lDSMC \
+    -lOpenFOAM -ldl -ltriSurface -lsurfMesh -lPstream -lfileFormats
---
+++ inputstream.h
@@ -0,0 +1,280 @@
+﻿#ifndef INPUTSTREAM_H
+#define INPUTSTREAM_H
+
+#include <vector>
+#include <algorithm>
+
+#include "InflowBoundaryModel.H"
+#include "polyMesh.H"
+#include "constants.H"
+#include "tetIndices.H"
+
+namespace Foam {
+
+template<class CloudType>
+class InputStream
+        :
+        public InflowBoundaryModel<CloudType>
+{
+
+    const scalar sqrtPi = sqrt(pi);
+
+    //Name of dict with molecular abundances
+    const word componentAbundancesDictName_ = "componentAbundances";
+
+    const word moleculePropertiesDicrName_ = "moleculeProperties";
+
+    const word massDictKey_ = "mass";
+
+    // Private Data
+
+    //- The indices of patches to introduce molecules across
+    labelList patches_;
+
+    //Names of the molecules
+    wordList componentNames_;
+
+    scalarList componentAbundances_;
+
+    scalarList componentMasses_;
+
+    labelList componentTypeId_;
+
+    void checkTemperature() const;
+
+    vector genMoleculeVelocity(
+            Random& rndGen,
+            const vector& n,
+            const vector& U,
+            double mass,
+            double T);
+
+public:
+
+    //- Runtime type information
+    TypeName("InputStream");
+
+
+    // Constructors
+
+    //- Construct from dictionary
+    InputStream(
+            const dictionary& dict,
+            CloudType& cloud);
+
+
+    //- Destructor
+    virtual ~InputStream();
+
+
+    // Member Functions
+
+    // Mapping
+
+    //- Remap the particles to the correct cells following mesh change
+    virtual void autoMap(const mapPolyMesh&);
+
+    //- Introduce particles
+    virtual void inflow();
+};
+
+template<class CloudType>
+void InputStream<CloudType>::checkTemperature() const
+{
+    const volScalarField::Boundary& boundaryT = this->owner().boundaryT().boundaryField();
+    forAll(patches_, p){
+        if(min(boundaryT[p]) < small){
+            FatalErrorInFunction
+                    << "Zero boundary temperature detected, check boundaryT "
+                    << "condition." << nl
+                    << nl << abort(FatalError);
+        }
+    }
+}
+
+template<class CloudType>
+vector InputStream<CloudType>::genMoleculeVelocity(
+        Random &rndGen,
+        const vector &n,
+        const vector &U,
+        double mass,
+        double T)
+{
+    //n - normal pointing inside
+    CloudType& cloud = this->owner();
+    scalar s = cloud.maxwellianMostProbableSpeed(T, mass) / sqrt(2.0);
+    scalar vInf = (n & U) < -3.0 * s ? s : (n & U) + 3.0 * s;
+    vector moleculeVelocity;
+    do
+    {
+        moleculeVelocity = s *
+                vector(rndGen.scalarNormal(), rndGen.scalarNormal(), rndGen.scalarNormal())
+                + U;
+    }
+    while((moleculeVelocity & n) / vInf < rndGen.scalar01());
+
+    return moleculeVelocity;
+}
+
+template<class CloudType>
+InputStream<CloudType>::InputStream(const dictionary &dict, CloudType &cloud)
+    :
+      InflowBoundaryModel<CloudType>(dict, cloud, typeName)
+{
+    DynamicList<label> patches;
+    forAll(cloud.mesh().boundaryMesh(), p){
+        const polyPatch& patch = cloud.mesh().boundaryMesh()[p];
+        if(isType<polyPatch>(patch)){
+            patches.append(p);
+        }
+    }
+    patches_.transfer(patches);
+
+    const dictionary& componentAbundances = this->coeffDict().subDict(
+                componentAbundancesDictName_);
+
+    componentNames_ = componentAbundances.toc();
+    componentAbundances_.setSize(componentNames_.size());
+    componentMasses_.setSize(componentNames_.size());
+    componentTypeId_.setSize(componentNames_.size());
+    const dictionary& moleculeProperties = cloud.particleProperties().subDict(
+                moleculePropertiesDicrName_);
+    Info << "Create custom properties of molecules for molecular inflow: " << nl;
+    forAll(componentNames_, n)
+    {
+        componentAbundances_[n] = componentAbundances.lookup<scalar>(componentNames_[n]);
+        componentMasses_[n] = moleculeProperties.subDict(componentNames_[n])
+                .lookup<scalar>(massDictKey_);
+        componentTypeId_[n] = findIndex(cloud.typeIdList(), componentNames_[n]);
+        Info << componentNames_[n] << " : "
+             << componentTypeId_[n] << " : "
+             << componentAbundances_[n] << " : "
+             << componentMasses_[n] << nl;
+    }
+
+    checkTemperature();
+}
+
+template<class CloudType>
+InputStream<CloudType>::~InputStream()
+{
+
+}
+
+template<class CloudType>
+void InputStream<CloudType>::inflow()
+{
+    CloudType& cloud = this->owner();
+
+    const polyMesh & mesh = cloud.mesh();
+
+    const scalar deltaT = mesh.time().deltaTValue();
+
+    Random & rndGen = cloud.rndGen();
+
+    label particlesInserted = 0;
+
+    const volScalarField::Boundary& boundaryT = cloud.boundaryT().boundaryField();
+
+    const volVectorField::Boundary& boundaryU = cloud.boundaryU().boundaryField();
+
+    const volScalarField::Boundary& rhoN = cloud.rhoN().boundaryField();
+
+    forAll(componentMasses_, componenti)
+    {
+        forAll(patches_, p)
+        {
+            const label patchi = patches_[p];
+
+            const polyPatch& patch = mesh.boundaryMesh()[patchi];
+
+            forAll(patch, facei){
+                const face& f = patch[facei];
+
+                label globalFaceIndex = facei + patch.start();
+
+                label celli = mesh.faceOwner()[globalFaceIndex];
+
+                const vector& fC = patch.faceCentres()[facei];
+
+                //small distance to put particle inside cell
+                const scalar smallStepInside = .0001 * mag(
+                            mesh.cells()[celli].centre(mesh.points(), mesh.faces()) - fC);
+
+                const vector n = - normalised(patch.faceAreas()[facei]);
+
+                const scalar fA = patch.magFaceAreas()[facei];
+
+                const scalar mostProbableSpeed = cloud.maxwellianMostProbableSpeed(
+                            boundaryT[patchi][facei],
+                            componentMasses_[componenti]);
+
+                const scalar sCosTheta = (boundaryU[patchi][facei] & n) / mostProbableSpeed;
+
+                List<tetIndices> faceTets = polyMeshTetDecomposition::faceTetIndices(
+                            mesh,
+                            globalFaceIndex,
+                            celli);
+
+                //Cumulative fractions to simulate random tet at face
+                static std::vector<scalar> faceTetsFractions;
+                faceTetsFractions.assign(faceTets.size(), 0.0);
+                scalar cumulativeSum = 0.0;
+                forAll(faceTets, faceTeti){
+                    cumulativeSum += faceTets[faceTeti].faceTri(mesh).mag() / fA;
+                    faceTetsFractions[faceTeti] = cumulativeSum;
+                }
+
+                scalar nP = fA
+                        * componentAbundances_[componenti] * rhoN[patchi][facei]
+                        * deltaT / cloud.nParticle()
+                        * mostProbableSpeed
+                        *(exp(-sqr(sCosTheta)) + sqrtPi*sCosTheta*(1 + erf(sCosTheta)))
+                        /(2.0*sqrtPi);
+
+                label numberOfParticles = rndGen.scalar01() > nP - label(nP) ?
+                            label(nP) : label(nP) + 1;
+
+                for(label i = 0; i < numberOfParticles; ++i){
+                    label triIdx = std::distance(faceTetsFractions.begin(),
+                                                 std::lower_bound(
+                                                     faceTetsFractions.begin(),
+                                                     faceTetsFractions.end(), rndGen.scalar01()));
+                    triIdx = triIdx == faceTets.size() ? triIdx - 1 : triIdx;
+
+                    const point r0 = faceTets[triIdx].faceTri(mesh).randomPoint(rndGen)
+                            + n * smallStepInside;
+
+                    const vector molecularVecolity = genMoleculeVelocity(
+                                rndGen,
+                                n,
+                                boundaryU[patchi][facei],
+                                componentMasses_[componenti],
+                                boundaryT[patchi][facei]);
+
+                    scalar Ei = cloud.equipartitionInternalEnergy(
+                                boundaryT[patchi][facei],
+                                cloud.constProps(componentTypeId_[componenti]).internalDegreesOfFreedom());
+
+                    cloud.addNewParcel(r0, celli, molecularVecolity, Ei, componentTypeId_[componenti]);
+
+                    particlesInserted++;
+                }
+            }
+        }
+    }
+
+    reduce(particlesInserted, sumOp<label>());
+
+    Info<< "    Particles inserted              = "
+        << particlesInserted << nl;
+}
+
+template<class CloudType>
+void InputStream<CloudType>::autoMap(const mapPolyMesh&) {
+    Info << "Just dummy yet" << nl;
+}
+
+}
+
+#endif // INPUTSTREAM_H
